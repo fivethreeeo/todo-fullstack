636960e8964dc7c4df856c476262133c72afd764
Sangbeom Heo <sangbeomheo@icloud.com>
2024-10-19 01:10:27 +0900

    cors 설정, 몽고디비아틀라스 연결

--- server/app.js
+++ server/app.js
@@ -1,13 +1,16 @@
 const express = require('express')
 const mongoose = require('mongoose')
+const cors = require('cors')
 const bodyParser = require('body-parser')
 const indexRouter = require('./routes/index')
+require('dotenv').config()
 
 const app = express()
 app.use(bodyParser.json())
+app.use(cors())
 app.use('/api', indexRouter)
 
-const mongoURI = 'mongodb://localhost:27017/todo'
+const mongoURI = process.env.MONGODB_URI_PROD
 
 mongoose
   .connect(mongoURI)
@@ -18,6 +21,6 @@ mongoose
     console.log('DB connection error: ', err)
   })
 
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log('Server is running on port 5000')
 })
